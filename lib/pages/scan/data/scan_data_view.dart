4ae505c05998fa7a5713c10d8b48bac895df9ee8
dhanasra <dhana.spiderlingz@gmail.com>
2024-04-25 16:17:06 +0530

    update: auto scan

--- lib/pages/scan/data/scan_data_view.dart
+++ lib/pages/scan/data/scan_data_view.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_neumorphic_plus/flutter_neumorphic.dart';
 import 'package:qrlingz_app/extensions/number_exten.dart';
 import 'package:qrlingz_app/extensions/string_exten.dart';
--- lib/pages/scan/data/scan_data_viewmodel.dart
+++ lib/pages/scan/data/scan_data_viewmodel.dart
@@ -1,12 +1,22 @@
 import 'package:qrlingz_app/base/base_viewmodel.dart';
 import 'package:qrlingz_app/models/qr_data.dart';
+import 'package:qrlingz_app/utils/utils.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+
+import '../../../utils/global.dart';
 
 class ScanDataViewModel extends BaseViewModel {
   final QRData qr;
-  ScanDataViewModel({required this.qr});
+  ScanDataViewModel({required this.qr}){
+    var dataType = getDataType(qr.data["value"]);
+    if(dataType=="Website" && Global.openLinkOnScan){
+      launchUrlString(qr.data['value']);
+    }
+  }
   
   @override
   dispose() {
+
   }
 
 }
