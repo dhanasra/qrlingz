7d5e6cdef3f43f832db139d744b5108a54e803a6
dhanasra <dhana.spiderlingz@gmail.com>
2024-06-07 21:26:29 +0530

    update: review item

--- lib/widgets/review_item.dart
+++ lib/widgets/review_item.dart
@@ -1,5 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:qrlingz_app/constants/color_const.dart';
 import 'package:qrlingz_app/extensions/number_exten.dart';
 import 'package:qrlingz_app/extensions/string_exten.dart';
 import 'package:qrlingz_app/network/models/review_data.dart';
@@ -26,56 +29,57 @@ class ReviewItem extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     CircleAvatar(
-                      child: "Email".bs(context),
+                      backgroundColor: ColorConst.colors[Random().nextInt(10)],
+                      child: (e.email?.substring(0, 1).toUpperCase()??'U').tl(context, color: Colors.white),
                     ),
                     Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.end,
                       children: [
-                        "1dhana625@gmail.com".bs(context),
-                      ],
-                    ),
-                  
-                    6.h(),
-                  
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        "Phone".bs(context),
-                        "unknown".bs(context),
+                        e.email?.ts(context),
+                        4.h(),
+                        e.phone?.bs(context),
+                        if(e.email==null && e.phone==null)
+                        "Unknown".tl(context)
                       ],
                     ),
                   ],
                 ),
                   
-                6.h(),
+                12.h(),
                 
-                "Categories".bs(context),
+                "Categories".tl(context),
+
+                12.h(),
                   
                 ...e.reviews.keys.map((key){
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                  
-                  
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          "$key".ts(context),
-                          RatingBarIndicator(
-                              rating: double.parse(e.reviews[key]['rating']),
-                              itemBuilder: (context, index) => const Icon(
-                                  Icons.star,
-                                  color: Colors.amber,
+                  return Padding(
+                    padding: const EdgeInsets.only(bottom: 4),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                    
+                    
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            "$key".bs(context),
+                            RatingBarIndicator(
+                                rating: double.parse(e.reviews[key]['rating']),
+                                itemBuilder: (context, index) => const Icon(
+                                    Icons.star,
+                                    color: Colors.amber,
+                                ),
+                                itemCount: 5,
+                                itemSize: 16.0,
+                                direction: Axis.horizontal,
                               ),
-                              itemCount: 5,
-                              itemSize: 16.0,
-                              direction: Axis.horizontal,
-                            ),
-                        ],
-                      ),
-                      
-                      "${e.reviews[key]['comment']}".ts(context)
-                    ],
+                          ],
+                        ),
+                        2.h(),
+                        
+                        "${e.reviews[key]['comment']}".bs(context)
+                      ],
+                    ),
                   );
                 })
                   
